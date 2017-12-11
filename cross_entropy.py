6806d8317a5a8b47f9e2c93166454853603b85db
kei52 <green9900grape@gmail.com>
2017-12-11 16:44:30 +0900

    modified

--- cross_entropy.py
+++ cross_entropy.py
@@ -12,16 +12,25 @@ def softmax(u):
     return e / np.sum(e)
 
 def forward(x):
-    global w1
-    global w2
-    u1 = x.dot(w1)
+    global W1
+    global W2
+    u1 = x.dot(W1)
+    print 'u1:',u1,u1.shape
     z1 = sigmoid(u1)
-    u2 = z1.dot(w2)
+    print 'z1:',z1,z1.shape
+    u2 = z1.dot(W2)
     y = softmax(u2)
     return y, z1
+'''
+x = np.array([[x1, x2]])
+となります。
 
-w1 = np.array([[0.1, 0.3], [0.2, 0.4]])
-w2 = np.array([[0.1, 0.3], [0.2, 0.4]])
+W1、W2はネットワークのパラメータの行列で、
+
+W1 = np.array([[w1_11, w1_21], [w1_12, w1_22]])
+'''
+W1 = np.array([[0.1, 0.3], [0.2, 0.4]])
+W2 = np.array([[0.1, 0.3], [0.2, 0.4]])
 
 x = np.array([[1, 0.5]])
 y, z1 = forward(x)
